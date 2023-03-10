efb5db885a42b90bd82c316931a740872a4bdad1
kellyaucoin <kelly.aucoin@kellogg.northwestern.edu>
2023-03-10 07:51:08 +0000

    continue with user authorization

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,13 @@
 class PlacesController < ApplicationController
 
   def index
+    @places = Place.where({ "user_id" => session["user_id"] })
     @places = Place.all
   end
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @posts = Post.where({ "place_id" => @place["id"] })
+    @posts = Post.where({ "place_id" => @place["id"], "user_id" => @current_user["id"]})
   end
 
   def new
@@ -14,10 +15,23 @@ class PlacesController < ApplicationController
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["place"]["name"]
-    @place.save
+    if @current_user
+      @place = Place.new
+      @place["name"] = params["place"]["name"]
+      @place.save
+    end
+    
     redirect_to "/places"
   end
+
+  def destroy
+    @place = Place.find_by({ "id" => params["id"] })
+    if @place["user_id"] == @current_user["id"]
+      @place.destroy
+    end
+
+    redirect_to "/places"
+  end
+
 
 end
